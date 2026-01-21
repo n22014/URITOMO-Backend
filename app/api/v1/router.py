90f26a18d3364b734935589267c69ae542f4c1e7
Ohjackson <wogus3673@naver.com>
2026-01-21 16:27:48 +0900

    feat: Implement API for starting live sessions and integrate into v1 router #33

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -8,6 +8,8 @@ from app.example.user.router import router as example_router
 from app.example.token.router import router as example_token_router
 from app.api.v1.user.main import router as main_router
 
+from app.meeting.api import router as meeting_router
+
 from app.core.token import security_scheme
 
 api_router = APIRouter()
@@ -18,6 +20,7 @@ api_router.include_router(example_router) # Includes login-debug
 # 2. Routes that DO need authentication (Protected)
 api_router.include_router(example_token_router, dependencies=[Depends(security_scheme)])
 api_router.include_router(main_router, dependencies=[Depends(security_scheme)])
+api_router.include_router(meeting_router, dependencies=[Depends(security_scheme)])
 
 
 
