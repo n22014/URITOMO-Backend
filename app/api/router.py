16d20d3ce636fb8414ede8b79b0972de9413f961
e1n22053 <e1n22053@oit.ac.jp>
2026-01-28 14:51:28 +0900

    feat: Add API endpoint for creating new rooms.

--- app/api/router.py
+++ app/api/router.py
@@ -10,6 +10,7 @@ from app.api.user.main import router as main_router
 from app.api.user.profile import router as profile_router
 
 from app.api.user.room_detail import router as room_detail_router
+from app.api.room.create import router as room_create_router
 from app.worker.worker_token import router as worker_token_router
 from app.api.user.friends import router as friends_router
 
@@ -31,6 +32,7 @@ api_router.include_router(worker_token_router)
 api_router.include_router(main_router, dependencies=[Depends(security_scheme)])
 
 api_router.include_router(room_detail_router, dependencies=[Depends(security_scheme)])
+api_router.include_router(room_create_router, dependencies=[Depends(security_scheme)])
 api_router.include_router(friends_router, dependencies=[Depends(security_scheme)])
 api_router.include_router(profile_router, dependencies=[Depends(security_scheme)])
 api_router.include_router(meeting_router, dependencies=[Depends(security_scheme)])
